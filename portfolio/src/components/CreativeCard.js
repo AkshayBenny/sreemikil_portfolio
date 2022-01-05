cd68f3c85bb7f931a7b12ef99706cb92ea54ba7a
AkshayBenny <akshaybennygh@gmail.com>
2022-01-05 23:46:31 +0530

    update

--- portfolio/src/components/CreativeCard.js
+++ portfolio/src/components/CreativeCard.js
@@ -1,11 +1,30 @@
 import React from 'react';
 
 function CreativeCard({ img, title, subTitle }) {
-  return <div className='bg-slate-300'>
-      <img src={img}  alt="img" />
-      <p>{title}</p>
-      <p>{subTitle}</p>
-  </div>;
+  return (
+    <div className='shadow-[0px 0px 20px rgba(201, 199, 199, 0.19)] p-[20px] md:p-[28px] border-2 rounded-[16px]  md:max-w-[552px] max-w-[363px] h-[426px] md:h-[504px]'>
+      <div className='object-cover'>
+        <img
+          className='rounded-[16px] h-[286.43px] md:h-[314px] pb-[16px] w-[323px] md:w-[496px]'
+          src={img}
+          alt='img'
+        />
+      </div>
+      <div>
+        <p className='pb-[5px] md:pb-[8px] font-semibold text-[20px] md:text-[32px]'>
+          {title}
+        </p>
+      </div>
+      <div>
+        <p className='pb-[16px] md:pb-[24px] font-normal text-[16px]'>
+          {subTitle}
+        </p>
+      </div>
+      <div>
+        <p className='font-semibold text-[16px] text-[#120055]'>Read More</p>
+      </div>
+    </div>
+  );
 }
 
 export default CreativeCard;
