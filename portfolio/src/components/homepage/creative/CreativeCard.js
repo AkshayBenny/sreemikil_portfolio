058eaa79605efd29320f0ccc9fdb0ae13b073efa
AkshayBenny <akshaybennygh@gmail.com>
2022-01-27 15:58:02 +0530

    back to top button created

--- portfolio/src/components/homepage/creative/CreativeCard.js
+++ portfolio/src/components/homepage/creative/CreativeCard.js
@@ -2,26 +2,26 @@ import React from 'react';
 
 function CreativeCard({ img, title, subTitle }) {
   return (
-    <div className='cursor-pointer transition-all	 shadow-[0px 0px 20px rgba(201, 199, 199, 0.19)] p-[20px] md:p-[28px] border-2 rounded-[16px]  md:max-w-[552px] max-w-[363px] hover:shadow-lg '>
+    <div className='shadow-[0px 0px	 20px rgba(201, 199, 199, 0.19)] max-w-[363px] cursor-pointer rounded-[16px] border-2 p-[20px] transition-all  hover:shadow-lg md:max-w-[552px] md:p-[28px] '>
       <div className='object-cover'>
         <img
-          className='rounded-[16px] mb-[16px] h-[195px] md:h-[314px] w-[323px] md:w-[496px]'
+          className='mb-[16px] h-[195px] w-[323px] rounded-[16px] md:h-[314px] md:w-[496px]'
           src={img}
           alt='img'
         />
       </div>
       <div>
-        <p className='pb-[5px] md:pb-[8px] font-semibold text-[20px] md:text-[32px]'>
+        <p className='pb-[5px] text-[20px] font-semibold md:pb-[8px] md:text-[32px]'>
           {title}
         </p>
       </div>
       <div>
-        <p className='pb-[16px] md:pb-[24px] font-normal text-[16px]'>
+        <p className='pb-[16px] text-[16px] font-normal md:pb-[24px]'>
           {subTitle}
         </p>
       </div>
       <div>
-        <p className='font-semibold text-[16px] text-[#120055]'>Read More</p>
+        <p className='text-[16px] font-semibold text-[#120055]'>Read More</p>
       </div>
     </div>
   );
